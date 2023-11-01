f41b85b661d489da70f7f60c3edae82458aad070
MarcMG00 <marc.moli20@gmail.com>
2023-11-01 10:20:10 +0100

    Add read Continents file

--- src/risk/modele/Continent.java
+++ src/risk/modele/Continent.java
@@ -8,4 +8,28 @@ public class Continent {
 	private ArrayList<Territoire> territoires;
 	private int valeurBonus;
 	
+	public Continent(int numContinent, String nomContinent, int valeurBonus, ArrayList<Territoire> territoires) {
+		this.numContinent = numContinent;
+		this.nomContinent = nomContinent;
+		this.valeurBonus = valeurBonus;
+		this.territoires = territoires;
+	}
+
+	public int getNumContinent() {
+		return numContinent;
+	}
+
+	public String getNomContinent() {
+		return nomContinent;
+	}
+
+	public ArrayList<Territoire> getTerritoires() {
+		return territoires;
+	}
+
+	public int getValeurBonus() {
+		return valeurBonus;
+	}
+	
+	
 }
--- src/risk/modele/Risk.java
+++ src/risk/modele/Risk.java
@@ -33,7 +33,12 @@ public class Risk {
 			String ligne;
 			while ((ligne = bufferedReader.readLine()) != null) {
 				String[] territoires = ligne.split(";");
-				this.territoires.add(new Territoire(Integer.parseInt(territoires[0]), territoires[1]));
+				if(this.territoires.size() == 42) {
+					break;
+				}
+				else {
+					this.territoires.add(new Territoire(Integer.parseInt(territoires[0]), territoires[1]));
+				}
 			}
 		}
 	    catch (IOException e) {
@@ -91,6 +96,50 @@ public class Risk {
 		return this.cartes;
 	}
 	
+	public ArrayList<Continent> lectureContinents(String fileName) {
+		ArrayList<Territoire> territoiresLL = lectureTerritoires("data/Territoires.txt");
+		ArrayList<String> territoiresContinentAReinitialiserStr = new ArrayList<String>();
+		ArrayList<Territoire> territoiresContinentAReinitialiserT = new ArrayList<Territoire>();
+		FileReader fileReader = null;
+	    BufferedReader bufferedReader = null;
+	    
+	    try {
+			fileReader = new FileReader(fileName);
+			bufferedReader = new BufferedReader(fileReader);
+			String ligne;
+			while ((ligne = bufferedReader.readLine()) != null) {
+				String[] continentL = ligne.split(";");
+				for(int iLStr = 3; iLStr < continentL.length; iLStr++) {
+					territoiresContinentAReinitialiserStr.add(continentL[iLStr]);
+				}
+				for(int iTerrit = 0; iTerrit < territoiresLL.size(); iTerrit++) {
+					if(territoiresContinentAReinitialiserStr.contains(territoiresLL.get(iTerrit).getNomTerritoire())) {
+						territoiresContinentAReinitialiserT.add(territoiresLL.get(iTerrit));
+					}
+				}
+				this.continents.add(new Continent(Integer.parseInt(continentL[0]), continentL[1], Integer.parseInt(continentL[2]), territoiresContinentAReinitialiserT));
+				territoiresContinentAReinitialiserStr = new ArrayList<String>();
+				territoiresContinentAReinitialiserT = new ArrayList<Territoire>();
+			}
+		}
+	    catch (IOException e) {
+			System.out.println("Exception � la lecture du fichier : " + e.getMessage());
+		}
+	    finally {
+			try {
+				if (fileReader != null) {
+					fileReader.close();
+				}
+				if (bufferedReader != null) {
+					bufferedReader.close();
+				}
+			} catch (IOException e) {
+				System.out.println("Exception survenue � la fermeture du fichier : " + e.getMessage());
+			}
+		}
+		return this.continents;
+	}
+	
 	public HashMap<Territoire, Territoire_Adjacent> lectureTerritoiresAdjacents(String fileName) {
 		ArrayList<Territoire> territoires = lectureTerritoires("data/Territoires.txt");
 		FileReader fileReader = null;
@@ -123,10 +172,6 @@ public class Risk {
 		return this.territoiresAdjacents;
 	}
 	
-	public ArrayList<Continent> lectureContinents(String fileName) {
-		return this.continents;
-	}
-	
 	public void lancerPartie() {
 		ArrayList<Territoire> territoiresF = lectureTerritoires("data/Territoires.txt");
 		for(Territoire t : territoiresF) {
@@ -137,6 +182,12 @@ public class Risk {
 		ArrayList<Carte_Territoire> cartesF = lectureCartesTerritoire("data/CartesTerritoires.txt");
 		for(Carte_Territoire ct : cartesF) {
 			System.out.println(ct.getNumCarte() + " " + ct.getTerritoire().getNomTerritoire() + " " + ct.getTypeRegiment());
+		}
+		System.out.println();
+		
+		ArrayList<Continent> continentsF = lectureContinents("data/Continents.txt");
+		for(Continent co : continentsF) {
+			System.out.println(co.getNumContinent() + " " + co.getNomContinent()  + " " + co.getValeurBonus() + " " + co.getTerritoires());
 		}
 		System.out.println();
 		
